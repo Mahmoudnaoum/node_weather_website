ea71ba2d9870511a10fb1e6c61a62df61ab00dc3
mahmoudnaoum <mahmoudnaoum13@gmail.com>
2021-11-03 12:49:26 +0200

    editing the forcast file

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (longitude, latitude, callback) => {
-    const url = 'http://api.weatherstack.com/current?access_key=a4e6ba0dd5cab5c959a288172f9452fa&query=' + latitude + ',' + longitude + '&units=f'
+    const url = 'http://api.weatherstack.com/current?access_key=a4e6ba0dd5cab5c959a288172f9452fa&query=' + latitude + ',' + longitude + '&units=m'
     request({ url:url , json: true}, (error, response) => {
         if(error){
             callback('Unable to connect to weather service!')
@@ -12,7 +12,7 @@ const forecast = (longitude, latitude, callback) => {
         else {
             
             const data = response.body
-            callback(undefined, data.current.weather_descriptions[0] + `. It is currently ${data.current.temperature} out.It feels like ${data.current.feelslike} degrees out.`)
+            callback(undefined, data.current.weather_descriptions[0] + `. It is currently ${data.current.temperature} out with humidity of ${data.current.humidity}%.It feels like ${data.current.feelslike} degrees out.`)
         }
         
     })
